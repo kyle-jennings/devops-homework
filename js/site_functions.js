d435265ce569d9fc51846e03beb05aa9aec18179
Ed Cupaioli <ecupaio@gmail.com>
2020-08-17 17:39:29 -0400

    added css and functions

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,3 +1,14 @@
 $(function(){
-
+  $.ajax({
+    url: 'https://geoip-db.com/json/',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      var location = data;
+      var city = location.city;
+      $('#user-location').text(city)
+      $('.location-vis').addClass('active');
+      
+    }
+  });
 });
